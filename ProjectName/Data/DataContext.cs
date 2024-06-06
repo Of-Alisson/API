4e1488e812b38a43c91bb6e125543595f79cd0b2
Alisson Lima <alissonlima50138@gmail.com>
2024-06-06 16:48:31 -0300

    commit new api

---
+++ ProjectName/Data/DataContext.cs
@@ -0,0 +1,7 @@
+namespace ProjectName.Data
+{
+    public class DataContext
+    {
+        
+    }
+}
---
+++ ProjectName/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectName.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cidade { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
